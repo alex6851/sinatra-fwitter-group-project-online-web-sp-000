7e43faf37e5eb28c6993e1151098c70f7b717ef6
alex6851 <alex.baker685@gmail.com>
2019-10-02 19:28:23 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+  end
+  get '/' do
+    erb :"application/index"
+  end
+
+  get '/signup' do
+    erb :"application/signup"
+  end
+
+  post '/signup' do 
+    binding.pry
+    if params[:username] != ""
+      user = User.new(:username => params[:username], :password => params[:password])
+    else
+      redirect "/failure"
+    end
+    if user.save
+      redirect "/tweets"
+    else
+      redirect "/failure"
+    end
+  end
 
   configure do
     set :public_folder, 'public'
